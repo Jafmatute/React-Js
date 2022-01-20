b1068a84c04e3953b28c02626f9fc6c46ab26927
Josue Ariel Flores Matute <josueari20@gmail.com>
2022-01-20 14:03:21 -0600

    Operador condicional ternario

---
+++ 02-intro-javascript/src/bases/12-ternarios.js
@@ -0,0 +1,16 @@
+/** @format */
+
+const activo = false;
+
+//let mensaje;
+
+// if (activo) {
+//   mensaje = "activo";
+// } else {
+//   mensaje = "inactivo";
+// }
+
+//formas
+const mensaje = activo ? "activo" : "inactivo";
+const mensaje2 = activo && "activo";
+console.log(mensaje2);
