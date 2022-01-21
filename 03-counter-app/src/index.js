2ba4d324ba02e5bef90b04110c185b38f8ded936
Josue Ariel Flores Matute <josueari20@gmail.com>
2022-01-21 15:01:33 -0600

    Hola Mundo en React

---
+++ 03-counter-app/src/index.js
@@ -0,0 +1,10 @@
+/** @format */
+
+import React from "react";
+import ReactDOM from "react-dom";
+const saludo = <h1>Hola Mundo</h1>;
+
+//Referencia
+const divRoot = document.querySelector("#root");
+
+ReactDOM.render(saludo, divRoot);
