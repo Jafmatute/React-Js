76f56e3e3e81ad5459bc862fa86d5ccb09efaa4d
Josue Ariel Flores Matute <josueari20@gmail.com>
2022-01-24 20:59:47 -0600

    Evento click (Eventos en general)

--- 03-counter-app/src/CounterApp.js
+++ 03-counter-app/src/CounterApp.js
@@ -4,10 +4,17 @@ import React from "react";
 import PropTypes from "prop-types";
 
 const CounterApp = ({ value }) => {
+  //HandleAdd
+  const handleAdd = (e) => {
+    console.log(e);
+  };
+
   return (
     <React.Fragment>
       <h1>CounterApp</h1>
       <h2>{value}</h2>
+
+      <button onClick={handleAdd}>+1</button>
     </React.Fragment>
   );
 };
