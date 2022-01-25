a50b0f5dd471199ce6b0ce54b42c7f827c453d7d
Josue Ariel Flores Matute <josueari20@gmail.com>
2022-01-24 21:22:50 -0600

    useState - Hook y handleSubtract y handleReset

--- 03-counter-app/src/CounterApp.js
+++ 03-counter-app/src/CounterApp.js
@@ -1,20 +1,38 @@
 /** @format */
 
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 
-const CounterApp = ({ value }) => {
+const CounterApp = ({ value = 10 }) => {
+  const [counter, setCounter] = useState(value); //[]
+
   //HandleAdd
   const handleAdd = (e) => {
     console.log(e);
+    // setCounter(counter + 1);
+
+    setCounter((c) => c + 1);
+  };
+
+  const handleReset = () => {
+    setCounter(value);
+  };
+  const handleDecrease = () => {
+    if (counter > 0) {
+      setCounter(counter - 1);
+    }
+
+    return;
   };
 
   return (
     <React.Fragment>
       <h1>CounterApp</h1>
-      <h2>{value}</h2>
+      <h2>{counter}</h2>
 
       <button onClick={handleAdd}>+1</button>
+      <button onClick={handleReset}>Reset</button>
+      <button onClick={handleDecrease}>-1</button>
     </React.Fragment>
   );
 };
