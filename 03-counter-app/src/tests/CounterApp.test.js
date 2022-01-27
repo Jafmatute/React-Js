810df4be091eea34205f06b57302daed651e45d7
Josue Ariel Flores Matute <josueari20@gmail.com>
2022-01-26 19:17:40 -0600

    Simular eventos - Click y Pruebas con el botón de reset

--- 03-counter-app/src/tests/CounterApp.test.js
+++ 03-counter-app/src/tests/CounterApp.test.js
@@ -4,9 +4,13 @@ import { shallow } from "enzyme";
 import CounterApp from "../CounterApp";
 
 describe("Pruebas en el archivo CounterApp", () => {
-  test("debe de mostrar <CounterApp /> correctamente", () => {
-    const wrapper = shallow(<CounterApp />);
+  //Inicialozar pruebas (ciclo de vida)
+  let wrapper;
 
+  beforeEach(() => {
+    wrapper = shallow(<CounterApp />);
+  });
+  test("debe de mostrar <CounterApp /> correctamente", () => {
     expect(wrapper).toMatchSnapshot();
   });
 
@@ -17,5 +21,29 @@ describe("Pruebas en el archivo CounterApp", () => {
     const textoParrafo = wrapper.find("h2").text().trim();
 
     expect(Number(textoParrafo)).toBe(valor);
+  });
+
+  test("Debe incrementar el contador con el bóton +1", () => {
+    wrapper.find("button").at(0).simulate("click");
+    const textoButtom = wrapper.find("h2").text().trim();
+
+    expect(Number(textoButtom)).toBe(11);
+  });
+
+  test("Debe decrementar el contador con el bóton -1", () => {
+    const textoButtom = wrapper.find("h2").text().trim();
+    wrapper.find("button").at(2).simulate("click");
+
+    expect(Number(textoButtom)).toBe(10);
+  });
+
+  test("Debe colocar el valor por defecto con el bóton reset", () => {
+    const wrapper = shallow(<CounterApp value={105} />);
+
+    wrapper.find("button").at(0).simulate("click");
+    wrapper.find("button").at(1).simulate("click");
+    const textoButtom = wrapper.find("h2").text().trim();
+
+    expect(Number(textoButtom)).toBe(105);
   });
 });
